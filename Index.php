7544ca54b2d79873c067c46777536a24c9303a47
Endorel <helene.francke@gmail.com>
2015-12-22 16:35:40 +0100

    ny test

---
+++ Index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+//LÄSER IN KLASSER
+function __autoload($class_name) {
+	include 'classes/'.$class_name.'.php';
+}
+
+//DATABASKOPPLINGEN
+$dbCon = Connection::connect();
+
+//NYA OBJEKT
+$user = new User();
+$print = new PrintPage();
+
+//OM MAN HAR TRYCKT PÅ LOGIN KNAPP VISAS DET LOGIN FORMULÄR
+if (isset($_POST['login'])) {
+	$user->login($dbCon);
+}
+
+//OM SESSION HAR SATT VISAS DET TITLE, USERS NAMN OCH LOGOUT FORMULÄR
+if (isset($_SESSION['username'])) {
+	$data = array(
+		'title' => 'Webbloppis',
+		'name' =>$print->printName($dbCon),
+		'logoutForm' =>$print->printLogoutForm()
+	);
+}
+//ANNARS VISAS DET TITLE OCH LOGIN FORMULÄR
+else {
+	$data = array(
+		'title' => 'Webbloppis',
+		'loginForm' =>$print->printLoginForm()
+	);
+}
+
+//OM MAN HAR TRYCKT PÅ LOGOUT KNAPP VISAS DET TITLE, LOGOUT MEDDELANDE OCH LOGIN FORMULÄR
+if (isset($_POST['logout'])) {
+	$data = array(
+		'title' => 'Webbloppis',
+		'logoutMsg' =>$user->logout(),
+		'loginForm' =>$print->printLoginForm()
+	);
+}
+
+//Läser in Twig
+require_once 'Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('Templates/');
+$twig = new Twig_Environment($loader);
+echo $twig->render('index.html', $data);
---
+++ webbloppis.sql
@@ -0,0 +1,35 @@
+-- Adminer 4.2.2 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `type_membership`;
+CREATE TABLE `type_membership` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `permission` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `type_membership` (`id`, `name`, `permission`) VALUES
+(1,	'Bronze',	''),
+(2,	'Silver',	''),
+(3,	'Gold',	'');
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `username` varchar(100) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL,
+  `date` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `type_membership_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `user` (`id`, `name`, `username`, `password`, `date`, `type_membership_id`) VALUES
+(1,	'Natalia',	'nat',	'1234',	'0000-00-00 00:00:00',	0);
+
+-- 2015-12-22 09:21:20
