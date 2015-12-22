34eb7c671c8f6bc5b37c630cbf15407e3bf84028
Natalia Nakagawa <Natalia Nakagawa>
2015-12-22 10:27:04 +0100

    med kommentarer

--- Index.php
+++ Index.php
@@ -1,17 +1,23 @@
 <?php
 session_start();
 
+//LÄSER IN KLASSER
 function __autoload($class_name) {
 	include 'classes/'.$class_name.'.php';
 }
 
+//DATABASKOPPLINGEN
 $dbCon = Connection::getInstance();
+
+//NYA OBJEKT
 $user = new User();
 $print = new PrintPage();
 
+//OM MAN INTE HAR TRYCKT PÅ LOGIN KNAPP VISAS LOGIN FORMULÄR
 if (!isset($_POST['login'])) {
 	$print->printLoginForm();
 }
+//ANNARS KOLLAR DET IFALL DET FINNS DEN USER I DATABASEN
 else {
 	$user->login($dbCon);
 }
