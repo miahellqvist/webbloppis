5cb87869730cd885baf28030fe8bbdd203420908
Natalia Nakagawa <Natalia Nakagawa>
2015-12-22 15:55:41 +0100

    tog bort while i User.php

--- classes/User.php
+++ classes/User.php
@@ -17,12 +17,10 @@ class User {
 		$result = $dbCon->query($query);
 
 		//OM USERNAME OCH PASSWORD STÄMMER SKAPAS EN SESSION OCH ETT NYTT OBJEKT FÖR ATT VISA USERS NAMN
-		if ($result->num_rows == 1) {
-			while ($row = $result->fetch_assoc()){
-				$_SESSION['username'] = $this->username;
-				$object = new PrintPage();
-				$object->printName($dbCon);
-			}
+		if ($row = $result->fetch_assoc()) {
+			$_SESSION['username'] = $this->username;
+			$object = new PrintPage();
+			$object->printName($dbCon);
 		} 
 		else {
 			echo "Wrong username or password";
