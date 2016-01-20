c96bca1f11aa690111b8cc4fe0743dffaed37d92
Endorel <helene.francke@gmail.com>
2016-01-20 09:26:09 +0100

    ny databas

--- webbloppis.sql
+++ webbloppis.sql
@@ -5,6 +5,10 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `webbloppis`;
+CREATE DATABASE `webbloppis` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `webbloppis`;
+
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -12,15 +16,15 @@ CREATE TABLE `category` (
   PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `category` (`category_id`, `category_name`) VALUES
-(1,	'Damkläder'),
-(2,	'Herrkläder'),
-(3,	'Barnkläder'),
-(4,	'Leksaker'),
-(5,	'Heminredning'),
-(6,	'Verktyg'),
-(7,	'Trädgård'),
-(8,	'Böcker');
+
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `city_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `state_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`city_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 
 DROP TABLE IF EXISTS `membership`;
 CREATE TABLE `membership` (
@@ -28,10 +32,6 @@ CREATE TABLE `membership` (
   `membership_limit` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `membership` (`membership_name`, `membership_limit`) VALUES
-('Guld',	500),
-('Silver',	50),
-('Brons',	10);
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
@@ -50,19 +50,6 @@ CREATE TABLE `product` (
   PRIMARY KEY (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `product` (`product_id`, `title`, `text`, `price`, `image_name`, `image_type`, `image_size`, `category`, `subcategory`, `date_added`, `user_id`, `state_id`) VALUES
-(5,	'Böcker i bokhylla',	'SÃ¤ljer alla mina bÃ¶cker och tillhÃ¶rande bokhylla. ',	1000,	'colinthompson.jpg',	'image/jpeg',	329238,	'8',	'17',	'2016-01-12 19:52:03',	5,	12),
-(6,	'Kattunge',	'SÃ¶t kattunge sÃ¤ljes till kÃ¤rleksfullt hem.',	500,	'Foto 2012-08-19 16 52 48.jpg',	'image/jpeg',	1705844,	'4',	'7',	'2016-01-11 09:49:40',	5,	12),
-(7,	'Tre glada pingviner',	'SÃ¤ljer mina tre pratglada och matglada pingviner. \r\n\r\nDe Ã¤ter 30 sillar om dagen/pingvin.',	7500,	'Penguins.jpg',	'image/jpeg',	777835,	'4',	'8',	'2016-01-05 16:21:52',	6,	0),
-(17,	't-shirt',	'snygg t-shirt',	50,	'nat/tshirt.jpg',	'image/jpeg',	631951,	'5',	'3',	'2016-01-05 15:33:17',	5,	0),
-(18,	'TrÃ¶t hund',	'jÃ¤ttecharmig och gullig hund',	1000,	'annaco/hund.jpg',	'image/jpeg',	113844,	'4',	'7',	'2016-01-05 15:40:01',	6,	0),
-(22,	'Monopply spel',	'fun!',	100,	'mia/monopoly.jpg',	'image/jpeg',	119249,	'4',	'11',	'2016-01-11 10:33:50',	7,	3),
-(23,	'Sko',	'Höga skor med klack',	400,	'mia/2516001035_orginal_3-96e32806.jpg',	'image/jpeg',	340576,	'1',	'3',	'2016-01-11 10:33:03',	7,	3),
-(24,	'Blå väska',	'En snygg blå väska',	456,	'mia/bbvaskalindex.jpg',	'image/jpeg',	73411,	'1',	'5',	'2016-01-11 10:24:25',	7,	1),
-(25,	'Plastblomma leksak',	'Hopp',	40,	'mia/Chrysanthemum.jpg',	'image/jpeg',	879394,	'4',	'5',	'2016-01-11 10:58:53',	7,	12),
-(26,	'Gräshoppa',	'Grön insekt',	34,	'mia/spe02302.jpg',	'image/jpeg',	623010,	'7',	'8',	'2016-01-11 14:13:15',	7,	0),
-(27,	'Boll',	'sdhja',	67,	'mia/Lighthouse.jpg',	'image/jpeg',	561276,	'4',	'2',	'2016-01-11 14:17:12',	7,	4),
-(29,	'glad hund',	'happy dog :)',	995,	'nat/gladhund.jpg',	'image/jpeg',	32814,	'4',	'8',	'2016-01-12 19:57:38',	5,	14);
 
 DROP TABLE IF EXISTS `rate`;
 CREATE TABLE `rate` (
@@ -71,10 +58,6 @@ CREATE TABLE `rate` (
   PRIMARY KEY (`rate_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `rate` (`rate_id`, `rate_name`) VALUES
-(1,	'Dålig säljare'),
-(2,	'Bra säljare'),
-(3,	'Utmärk säljare');
 
 DROP TABLE IF EXISTS `review`;
 CREATE TABLE `review` (
@@ -88,9 +71,6 @@ CREATE TABLE `review` (
   PRIMARY KEY (`review_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `review` (`review_id`, `buyer_id`, `buyer_name`, `seller_id`, `rate_id`, `comment`, `date_comment`) VALUES
-(90,	6,	'Anna',	5,	2,	':)',	'2016-01-14 22:52:43'),
-(92,	5,	'Natalia',	6,	2,	'Bra!',	'2016-01-15 00:05:30');
 
 DROP TABLE IF EXISTS `select_category`;
 CREATE TABLE `select_category` (
@@ -98,36 +78,6 @@ CREATE TABLE `select_category` (
   `subcategory_id` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `select_category` (`category_id`, `subcategory_id`) VALUES
-(1,	1),
-(1,	2),
-(1,	3),
-(1,	4),
-(2,	1),
-(2,	2),
-(2,	3),
-(2,	4),
-(3,	1),
-(3,	2),
-(3,	3),
-(3,	4),
-(4,	7),
-(4,	8),
-(4,	9),
-(4,	10),
-(4,	11),
-(1,	11),
-(2,	11),
-(3,	11),
-(5,	12),
-(5,	13),
-(5,	14),
-(5,	11),
-(7,	15),
-(7,	16),
-(7,	11),
-(8,	17),
-(8,	18);
 
 DROP TABLE IF EXISTS `state`;
 CREATE TABLE `state` (
@@ -136,28 +86,6 @@ CREATE TABLE `state` (
   PRIMARY KEY (`state_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `state` (`state_id`, `state_name`) VALUES
-(1,	'Blekinge län'),
-(2,	'Dalarnas län'),
-(3,	'Gotlands län'),
-(4,	'Gävleborgs län'),
-(5,	'Hallands län'),
-(6,	'Jämtlands län'),
-(7,	'Jönköpings län'),
-(8,	'Kalmar län'),
-(9,	'Kronobergs län'),
-(10,	'Norrbottens län'),
-(11,	'Skåne län'),
-(12,	'Stockholms län'),
-(13,	'Södermanlands län'),
-(14,	'Uppsala län'),
-(15,	'Värmlands län'),
-(16,	'Västerbottens län'),
-(17,	'Västernorrlands län'),
-(18,	'Västmanlands län'),
-(19,	'Västra Götalands län'),
-(20,	'Örebro län'),
-(21,	'Östergötlands län');
 
 DROP TABLE IF EXISTS `subcategory`;
 CREATE TABLE `subcategory` (
@@ -166,25 +94,15 @@ CREATE TABLE `subcategory` (
   PRIMARY KEY (`subcategory_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `subcategory` (`subcategory_id`, `subcategory_name`) VALUES
-(1,	'Byxor'),
-(2,	'Tröjor'),
-(3,	'Skor'),
-(4,	'Ytterkläder'),
-(5,	'Redskap'),
-(6,	'Övrigt'),
-(7,	'Mjukisdjur'),
-(8,	'Uteleksaker'),
-(9,	'Spel'),
-(10,	'Dockor'),
-(11,	'Övrigt'),
-(12,	'Ljusstakar'),
-(13,	'Textil'),
-(14,	'Husgeråd'),
-(15,	'Frön'),
-(16,	'Redskap'),
-(17,	'Inbundna böcker'),
-(18,	'Pocket');
+
+DROP TABLE IF EXISTS `type_membership`;
+CREATE TABLE `type_membership` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `permission` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -204,13 +122,5 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `user` (`user_id`, `name`, `username`, `password`, `adress`, `zip_code`, `city`, `state`, `email`, `phone`, `date`, `type_membership_id`, `rate_id`) VALUES
-(5,	'Natalia',	'nat',	'$2y$11$e.KXqNPo.cyOajF6KF/i/.IaGnmktdoWcj1lT/ZBPv60fxGxErUzu',	'Sveavägen 41',	77788,	'Stockholm',	'12',	'natalianakagawa@gmail.com',	0,	'2016-01-15 19:29:30',	'Silver',	1),
-(6,	'Anna',	'annaco',	'$2y$11$m04JBtZetrgFrb6hlp6oLOxyFaxUsTV1YD0GHxIVCh6VE5.uRLJqe',	'VÃ¤gen 10',	222,	'Stockholm',	'12',	'anna@exempel.se',	0,	'2016-01-04 01:45:43',	'Brons',	1),
-(7,	'Maria',	'mia',	'$2y$11$D/paABdgp7PotP3Jcmrdaes7.fhjU71iC9Omijk.vybZ6kL142vr2',	'VÃ¤gen 9',	11144,	'Stockholm',	'3',	'mia@exempel.se',	0,	'2016-01-12 10:26:35',	'Silver',	1),
-(8,	'olles',	'olle',	'$2y$11$.dxfmne5/36Y9cbF9OXqeOiV6FCZS2oft.zy67rSsc6rPYNii5X1O',	'Gata vägen 4',	19287,	'Karlstad',	'5',	'hsafk@sdkl.se',	932042043,	'2016-01-12 10:23:38',	'Brons',	1),
-(9,	'stina',	'stina',	'$2y$11$zDeeWCRL2cU5Or05NZzI5.sKjFewUHPdwxoh85OCzc0I.nNgf4ObG',	'Karlaägaen',	48930,	'fjalfha',	'7',	'sfakl@dsf.se',	438902,	'2016-01-11 11:10:48',	'Brons',	1),
-(10,	'Carin',	'carin',	'$2y$11$/GgWI92mtUoU./zIaNo0beztUVs5p.xF1SHElwYvplprHJ3Y40l8C',	'Gatan 1',	123,	'Stockholm',	'12',	'a@mail.com',	1248998,	'2016-01-12 09:25:00',	'Silver',	1),
-(11,	'Linus',	'linus',	'$2y$11$bK.w99ibhVyr1qxX.gQ3AeA5cXdF5na5LTJoq3gLHOjO3Jn6iTrSC',	'Gatan 1',	12269,	'Malmö',	'11',	'a@mail.com',	25999999,	'2016-01-12 10:25:44',	'Brons',	1);
 
--- 2016-01-16 15:15:02
+-- 2016-01-20 08:23:37
